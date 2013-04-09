80a6a2b735f46669acb01a3c4b5552d80cfb077c
吴佳雷 <rockywu@anjuke.com>
2013-04-09 14:05:46 +0800

    添加css文件的tags

--- vim/vimfiles/lookupfiletags.sh
+++ vim/vimfiles/lookupfiletags.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # generate tag file for lookupfile plugin
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > lookupfiletags
-find $1 -regex '.*\.\(php\|phtml\|js\|html\|html\.twig\)' -type f -printf "%f\t%p\t1\n" | \
+find $1 -regex '.*\.\(php\|phtml\|js\|html\|html\.twig\|css\)' -type f -printf "%f\t%p\t1\n" | \
 sort -f >> lookupfiletags
