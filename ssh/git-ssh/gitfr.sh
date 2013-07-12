189daf878fb54309f1bc542003099a4f49c9dd74
吴佳雷 <rockywu@anjuke.com>
2013-07-12 10:08:46 +0800

    修改了git-ssh 的使用逻辑

--- ssh/git-ssh/gitfr.sh
+++ ssh/git-ssh/gitfr.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-git fetch --all
-if [ -z $1 ] && [ -z $2 ];then
-    git rebase origin/master
-elif [ -n $1 ] && [ -z $2 ];then
+if [ $# -eq 2 ];then
+    git fetch $1 $2
+    git rebase $1/$2
+elif [ $# -eq 1 ];then
+    git fetch origin $1
     git rebase origin/$1
 else
-    git rebase $1/$2
+    git fetch origin master
+    git rebase origin/master
 fi
+
+#git fetch --all
+#if [ -z $1 ] && [ -z $2 ];then
+#    git rebase origin/master
+#elif [ -n $1 ] && [ -z $2 ];then
+#    git rebase origin/$1
+#else
+#    git rebase $1/$2
+#fi
 
--- ssh/git-ssh/gitps.sh
+++ ssh/git-ssh/gitps.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-if [ -z $1 ] && [ -z $2 ];then
-    git push origin master:master
-elif [ -n $1 ] && [ -z $2 ];then
+if [ $# -eq 2 ];then
+    /bin/bash /home/rockywu/git-ssh/gitfr.sh $1 $2
+    git push $1 $2:$2
+elif [ $# -eq 1 ];then
+    /bin/bash /home/rockywu/git-ssh/gitfr.sh $1
     git push origin $1:$1
 else 
-    git push $1 $2:$2
+    /bin/bash /home/rockywu/git-ssh/gitfr.sh 
+    git push origin master:master
 fi
+
+#if [ -z $1 ] && [ -z $2 ];then
+#    git push origin master:master
+#elif [ -n $1 ] && [ -z $2 ];then
+#    git push origin $1:$1
+#else 
+#    git push $1 $2:$2
+#fi
 
