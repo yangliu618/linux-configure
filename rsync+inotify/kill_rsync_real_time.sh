3998cb6fd10299d5d132d84670184fe639970860
吴佳雷 <rockywu@anjuke.com>
2013-07-30 11:18:27 +0800

    添加进程管理监控,用于kill 同步进程

---
+++ rsync+inotify/kill_rsync_real_time.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a name
+name[0]="rsync_real_time_module.sh"
+name[1]="rsync_real_time_path.sh"
+for k in ${name[@]};do
+    num=`ps -ef | grep $k | grep -v "grep" | wc -l`
+    if [ $num -gt 0 ];then
+        process_id=`ps -ef | grep $k | grep -v "grep" | awk '{print $2}'`
+        for kill_id in $process_id;do
+            sudo kill $kill_id
+        done
+    fi
+done
