5735f945495519e0ac5fcdda8c6dc69b860bb10f
吴佳雷 <rockywu@anjuke.com>
2013-08-01 09:15:29 +0800

    编辑快捷gitfr.sh & gitps.sh

--- ssh/git-ssh/gitfr.sh
+++ ssh/git-ssh/gitfr.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
+git fetch --all
 if [ $# -eq 2 ];then
-    git fetch $1 $2 : $2
     git rebase $1/$2
 elif [ $# -eq 1 ];then
-    git fetch origin $1
     git rebase origin/$1
 else
-    git fetch origin master
     git rebase origin/master
 fi
-
-#git fetch --all
-#if [ -z $1 ] && [ -z $2 ];then
-#    git rebase origin/master
-#elif [ -n $1 ] && [ -z $2 ];then
-#    git rebase origin/$1
-#else
-#    git rebase $1/$2
-#fi
-
--- ssh/git-ssh/gitps.sh
+++ ssh/git-ssh/gitps.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+git fetch --all
 if [ $# -eq 2 ];then
-    /bin/bash /home/rockywu/git-ssh/gitfr.sh $1 $2
+    git rebase $1/$2
     git push $1 $2:$2
 elif [ $# -eq 1 ];then
-    /bin/bash /home/rockywu/git-ssh/gitfr.sh $1
+    git rebase origin/$1
     git push origin $1:$1
 else 
-    /bin/bash /home/rockywu/git-ssh/gitfr.sh 
+    git rebase origin/master
     git push origin master:master
 fi
-
-#if [ -z $1 ] && [ -z $2 ];then
-#    git push origin master:master
-#elif [ -n $1 ] && [ -z $2 ];then
-#    git push origin $1:$1
-#else 
-#    git push $1 $2:$2
-#fi
 
