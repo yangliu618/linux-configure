1d31463fb788e17eab9becdaa473ee1b93fd063d
rockywu 吴佳雷 <rockywu@anjuke.com>
2013-10-21 09:55:30 +0800

    添加执行文案

--- git/git-shell/gitfr.sh
+++ git/git-shell/gitfr.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo ">>>>>>>Run : git fetch --all"
 git fetch --all
 if [ $# -eq 2 ];then
+    echo ">>>>>>>Run : git rebase $1/$2"
     git rebase $1/$2
 elif [ $# -eq 1 ];then
+    echo ">>>>>>>Run : git rebase origin/$1"
     git rebase origin/$1
 else
+    echo ">>>>>>>Run : git rebase origin/master"
     git rebase origin/master
 fi
