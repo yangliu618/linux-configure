6c211766d43ca313d86d27c08a4deebdaee32fb7
rockywu 吴佳雷 <rockywu@anjuke.com>
2013-12-06 12:48:36 +0800

    添加g++编译

--- shell/g++.sh
+++ shell/g++.sh
@@ -4,9 +4,10 @@ echo ">>>>>>>Run : g++ execute start"
 #$2 编辑生成的执行文件
 #$3 编辑生成的执行路径
 if [ $# -eq 3 ];then
-    g++ "$1" -o "$3$2"
-    echo ">>>>>>>Run : g++ $1 -o $3$2"
-if [ $# -eq 2 ];then
+    echo "asdf"
+    g++ "$2" -o "$1$3"
+    echo ">>>>>>>Run : g++ $2 -o $1$3"
+elif [ $# -eq 2 ];then
     g++ "$1" -o "$2"
     echo ">>>>>>>Run : g++ $1 -o $2"
 elif [ $# -eq 1 ];then
