6181e6afb5d3210c3d5f45e54e9d2d43d4eb1f9b
rockywu 吴佳雷 <rockywu@anjuke.com>
2014-04-11 09:47:14 +0800

    添加重启kfsrestart.sh

---
+++ shell/kfsrestart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x 
+
+gitdir=/home/www/kfstouch/
+
+if [ ! -d $gitdir ];then
+    echo "$gitdir is no exists"
+    exit;
+fi
+port=8080;
+cd $gitdir;
+nohup /usr/bin/mvn clean jetty:run -Dajf.config.path=gitdir -Djetty.port=$port > /tmp/start.$port.log 2>&1 &
+echo "starting ..."
+echo $?
+
