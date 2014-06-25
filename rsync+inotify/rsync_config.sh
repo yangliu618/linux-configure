7b099bdca19b765beae1134f915990a10a64e1ca
rockywu 吴佳雷 <rockywu@anjuke.com>
2014-06-25 13:02:06 +0800

    添加FTP工具

--- rsync+inotify/rsync_config.sh
+++ rsync+inotify/rsync_config.sh
@@ -5,12 +5,11 @@ if ! [ $# -eq 1 ];then
     echo "没有可用的配置信息";
     exit;
 elif [ $1 -eq 0 ];then
-    sync[0]='/home/www/aifang/site/,root@10.10.3.208:/home/www/source/aifang/aifang-site' 
-    sync[1]='/home/www/aifang/pages/,root@10.10.3.208:/home/www/source/aifang/aifang-pages' 
+    sync[0]='' 
 elif [ $1 -eq 1 ];then
-    sync[2]='/home/www/aifang/site/,www-data@192.168.1.77:/var/www/rockywu/aifang/site' 
-    sync[3]='/home/www/aifang/pages/,www-data@192.168.1.77:/var/www/rockywu/aifang/pages' 
-    sync[4]='/home/www/aifang/selfindex/,www-data@192.168.1.77:/var/www/rockywu/aifang/selfindex' 
+    sync[2]='/home/www/aifang/kfs/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/kfs' 
+    sync[3]='/home/www/aifang/twpages/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/twpages' 
+    sync[4]='/home/www/aifang/system/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/system' 
 else
     echo "没有可用的配置信息";
     exit;
