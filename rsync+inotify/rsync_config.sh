7d7f0f98bdfbdc867aa13710fb3662483a5ada3b
rockywu 吴佳雷 <rockywu@anjuke.com>
2014-08-05 10:31:39 +0800

    添加修改配置

--- rsync+inotify/rsync_config.sh
+++ rsync+inotify/rsync_config.sh
@@ -7,6 +7,7 @@ if ! [ $# -eq 1 ];then
 elif [ $1 -eq 0 ];then
     sync[0]='' 
 elif [ $1 -eq 1 ];then
+    sync[1]='/home/www/aifang/pages/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/pages' 
     sync[2]='/home/www/aifang/kfs/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/kfs' 
     sync[3]='/home/www/aifang/twpages/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/twpages' 
     sync[4]='/home/www/aifang/system/,evans@192.168.1.167:/home/www/releases/php/rockywu/kfs/system' 
